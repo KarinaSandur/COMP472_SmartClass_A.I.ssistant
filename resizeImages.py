9a7328a226e4042bddb09292ecaf7d15f55ce8c3
Karina Sanchez <karinasd007@gmail.com>
2024-05-24 14:22:41 -0400

    Created function to resize images in folder

---
+++ resizeImages.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+# Function that iterates through files in folder and resizes them
+def resizeImages(folder, width, height):
+    for file in os.listdir(folder):
+        
+        # Resizes image
+        image = Image.open(os.path.join(folder, file))
+        new_image = image.resize((width, height))
+        new_image.save(os.path.join(folder, file))
+
+        # Prints Success Statement
+        print(f"Resized {file}")
+
+
+if __name__ == "__main__":
+    input_folder = input("Enter the folder path: ")
+    resizeImages(input_folder, 600, 600)
