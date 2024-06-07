ac4e48b2270c0af7132749c25191bcdc8128e765
nadiaaab <nadiabeauu@gmail.com>
2024-06-07 15:52:43 -0400

    fix

--- cnn_model.py
+++ cnn_model.py
@@ -13,9 +13,9 @@ import tempfile
 class MainModel(nn.Module):
     def __init__(self):
         super(MainModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=2, stride=1, padding=1)
         #modify kernel size here
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=1, padding=0)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1)
         self.conv_output_size = self._get_conv_output_size()
         self.fc1 = nn.Linear(self.conv_output_size, 128)
@@ -40,11 +40,11 @@ class MainModel(nn.Module):
 class Variant1(nn.Module):
         def __init__(self):
             super(Variant1, self).__init__()
-            self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+            self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)
             # modify kernel size here
-            self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=2)
-            self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-            self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+            self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+            self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
             self.conv_output_size = self._get_conv_output_size()
             self.fc1 = nn.Linear(self.conv_output_size, 256)
             self.fc2 = nn.Linear(256, 4)
@@ -70,11 +70,11 @@ class Variant1(nn.Module):
 class Variant2(nn.Module):
     def __init__(self):
         super(Variant2, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5, stride=2, padding=1)
         # modify kernel size here
-        self.pool = nn.MaxPool2d(kernel_size=5, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=5, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=5, stride=2, padding=1)
         self.conv_output_size = self._get_conv_output_size()
         self.fc1 = nn.Linear(self.conv_output_size, 256)
         self.fc2 = nn.Linear(256, 4)
