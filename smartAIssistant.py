7f95c9e475394d94733198798a406897c5d1299e
Karina Sanchez <karinasd007@gmail.com>
2024-06-12 23:25:59 -0400

    Predict class

--- smartAIssistant.py
+++ smartAIssistant.py
@@ -13,3 +13,32 @@ model.load_state_dict(torch.load("best_performing_model.pth"))
 # Set the model to evaluation mode
 model.eval()
 
+# Get image path
+image_path = input("Enter image path: ")
+image = Image.open(image_path)
+
+# Transform image to correct size
+transform = transforms.Compose([
+    transforms.Resize((150, 150)),
+    transforms.ToTensor()
+])
+
+# Apply transformation to image
+transformed_image = transform(image).unsqueeze(0)
+
+# Get class prediction from model
+with torch.no_grad():
+    output = model(transformed_image)
+    probabilities = torch.softmax(output, dim=1)
+    predicted_class = torch.argmax(probabilities)
+
+# Print class prediction
+if (predicted_class.item() == 0):
+    print(f"Predicted class: angry")
+if (predicted_class.item() == 1):
+    print(f"Predicted class: focused")
+if (predicted_class.item() == 2):
+    print(f"Predicted class: happy")
+if (predicted_class.item() == 3):
+    print(f"Predicted class: happy")
+
