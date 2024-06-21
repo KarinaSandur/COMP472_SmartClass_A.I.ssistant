7e5c3660116366cb913f652f68aabb5c868cac0f
Karina Sanchez <karinasd007@gmail.com>
2024-06-21 14:06:12 -0400

    Obtain metrics for different genders

--- predict_bias.py
+++ predict_bias.py
@@ -31,10 +31,19 @@ def count_files_in_folders(directory):
 
 
 if __name__ == "__main__":
-    data_dir = input("your_directory_path_here: ")
+
+    # Obtain folder paths where data is located from user
+    clean_data_age = input("Enter the path where you Clean Data Age folder is located: ")
+    clean_data_gender = input("Enter the path where you Clean Data Gender folder is located: ")
     batch_size = 32
 
     # count_files_in_folders(data_dir)
+
+    # Input paths where data is located
+    paths = {
+        clean_data_age,
+        clean_data_gender,
+    }
 
     # initialize model
     mainModel = cnn_model.MainModel()
@@ -42,51 +51,55 @@ if __name__ == "__main__":
     # load model
     mainModel.load_state_dict(torch.load("best_model_MainModel.pth"))
 
+    # Declare results
     results = {}
-        
-    with tempfile.TemporaryDirectory() as temp_dir:
-        for folder in os.listdir(data_dir):
-            folder_path = os.path.join(data_dir, folder)
-            if os.path.isdir(folder_path):
-                cnn_model.unzip_files(folder_path, temp_dir)
-                print(f"Unzipped files from {folder_path} to {temp_dir}")
-                dataset = cnn_model.load_data(temp_dir)
 
-                # split dataset into training 70%, validation 15%, and testing 15%
-                train_data, test_data = train_test_split(dataset, test_size=0.3, random_state=42)
-                val_data, test_data = train_test_split(test_data, test_size=0.5, random_state=42)
-                
-                # create data loaders for training, validation, and testing sets
-                train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-                val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
-                test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+    for data_dir in paths:  
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for folder in os.listdir(data_dir):
+                folder_path = os.path.join(data_dir, folder)
+                if os.path.isdir(folder_path):
+                    cnn_model.unzip_files(folder_path, temp_dir)
+                    print(f"Unzipped files from {folder_path} to {temp_dir}")
+                    dataset = cnn_model.load_data(temp_dir)
 
-                # for name, model in models.items():
-                mainModel.eval()
-                y_true = []
-                y_pred = []
+                    # split dataset into training 70%, validation 15%, and testing 15%
+                    train_data, test_data = train_test_split(dataset, test_size=0.3, random_state=42)
+                    val_data, test_data = train_test_split(test_data, test_size=0.5, random_state=42)
+                    
+                    # create data loaders for training, validation, and testing sets
+                    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+                    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+                    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
-                with torch.no_grad():
-                    for inputs, labels in test_loader:
-                        outputs = mainModel(inputs)
-                        _, predicted = torch.max(outputs, 1)
-                        y_true.extend(labels.numpy())
-                        y_pred.extend(predicted.numpy())
+                    # for name, model in models.items():
+                    mainModel.eval()
+                    y_true = []
+                    y_pred = []
 
-                # calculate macro metrics
-                accuracy = accuracy_score(y_true, y_pred)
-                precision_macro = precision_score(y_true, y_pred, average='macro')
-                recall_macro = recall_score(y_true, y_pred, average='macro')
-                f1_macro = f1_score(y_true, y_pred, average='macro')
+                    with torch.no_grad():
+                        for inputs, labels in test_loader:
+                            outputs = mainModel(inputs)
+                            _, predicted = torch.max(outputs, 1)
+                            y_true.extend(labels.numpy())
+                            y_pred.extend(predicted.numpy())
 
-                results[folder] = {
-                    'accuracy': accuracy,
-                    'precision_macro': precision_macro,
-                    'recall_macro': recall_macro,
-                    'f1_macro': f1_macro,
-                }
+                    # calculate macro metrics
+                    accuracy = accuracy_score(y_true, y_pred)
+                    precision_macro = precision_score(y_true, y_pred, average='macro')
+                    recall_macro = recall_score(y_true, y_pred, average='macro')
+                    f1_macro = f1_score(y_true, y_pred, average='macro')
 
-                print(results)
+                    results[folder] = {
+                        'accuracy': accuracy,
+                        'precision_macro': precision_macro,
+                        'recall_macro': recall_macro,
+                        'f1_macro': f1_macro,
+                    }
+
+    print(results)
+    print("\n")
+    print("\n")
     
 
     # Results from different age groups
@@ -108,6 +121,20 @@ if __name__ == "__main__":
     senior_recall_macro = round(senior_info.get("recall_macro"), 4)
     senior_f1_macro = round(senior_info.get("f1_macro"), 4)
 
+    # Results from different genders
+    men_info = results.get("men",{})
+    men_accuracy = round(men_info.get("accuracy"), 4)
+    men_precision_macro = round(men_info.get("precision_macro"), 4)
+    men_recall_macro = round(men_info.get("recall_macro"), 4)
+    men_f1_macro = round(men_info.get("f1_macro"), 4)
+
+    women_info = results.get("women",{})
+    women_accuracy = round(women_info.get("accuracy"), 4)
+    women_precision_macro = round(women_info.get("precision_macro"), 4)
+    women_recall_macro = round(women_info.get("recall_macro"), 4)
+    women_f1_macro = round(women_info.get("f1_macro"), 4)
+
+
     # Calculating Averages for Age Group Metrics
     age_accuracy_avg = round(((young_accuracy + middle_aged_accuracy + senior_accuracy)/3), 4)
     age_precision_avg = round(((young_precision_macro + middle_aged_precision_macro + senior_precision_macro)/3), 4)
@@ -121,6 +148,8 @@ if __name__ == "__main__":
         ["Middle-Aged", "10", middle_aged_accuracy, middle_aged_precision_macro, middle_aged_recall_macro, middle_aged_f1_macro],
         ["Senior", "10", senior_accuracy, senior_precision_macro, senior_recall_macro, senior_f1_macro],
         ["Total/Average", "30", age_accuracy_avg, age_precision_avg, age_recall_avg, age_f1_avg],
+        ["Male", "10", men_accuracy, men_precision_macro, men_recall_macro, men_f1_macro],
+        ["Female", "10", women_accuracy, women_precision_macro, women_recall_macro, women_f1_macro],
     ]
 
     # Create table
